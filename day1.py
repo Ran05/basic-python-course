695b3e83d08004eca10d0d979e6a72c6b738d328
Ran05 <ferarizarandolfh.ifliba@gmail.com>
2021-09-21 07:49:04 +0800

    day-1
    python dict workshop

---
+++ day1.py
@@ -0,0 +1,135 @@
+'''print("hello,World")'''
+# This is comment
+"This is comment"
+
+
+#Identation
+'''x = 1
+if x: 
+    print("yes")'''
+
+# variables
+
+# x = 5
+# name = "John Doe"
+# print(name)
+
+
+# Dynamic Typing
+# x=5
+# x = "John Doe"
+# print(x)
+
+
+# print(type(x))
+
+
+
+# Type casting
+
+# x = 7.00
+# x = str(7)
+# print(type(x))
+
+
+
+#assigning multiple values to several variables 
+'''
+x,y,z = "red","white","yellow"
+
+print(x,y,z)
+'''
+
+
+#assigning value to multiple 
+
+
+#global variables defining functions
+
+'''
+x = "global x"
+def test():
+    x= "local x"
+    print(x)
+
+test()
+
+print(x)
+
+
+'''
+
+
+#using the global keyword 
+'''
+x = 10
+
+
+def test():
+    global x 
+    x=20 # inappend natin yung value na 20 sa x
+    print(x)
+
+test()
+
+print(x)
+
+print(x)
+'''
+
+
+
+#Operators
+
+
+'''name = "John Doe"'''
+'''age =40
+
+
+print("Age is " + str(age))
+print(type(age))'''
+
+
+#The use of plus operator 
+
+'''x=10
+y=10
+
+print(int(x) + int(y))'''
+
+
+#my solutions
+'''x = int(10)
+y = int(10)
+
+
+print(x + y)'''
+
+
+
+#inputting and outputting variables the function input 
+
+
+name = input("Enter Full Name: ")
+email = input("Enter Email: ")
+
+
+print("Name:" + name)
+print("Email:" + email)
+
+
+
+
+
+
+#Activity 
+#coding challenge today 09-20-2021
+
+
+
+
+
+
+
+
+
---
+++ day1_act2.py
@@ -0,0 +1,12 @@
+name = input("Enter Your Name:")
+mathGrade = input("Enter Your Grade:")
+scieGrade = input("Enter Your Science Grade:")
+engGrade = input("Enter Your English Grade: ")
+
+sum = float(mathGrade) + float(scieGrade) + float(engGrade)
+
+average = sum / 3
+
+
+print("Your average is" + " "+ str(average))
+
